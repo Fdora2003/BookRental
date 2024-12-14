2b831298a732a284f97e42fdb9d4f03b52636989
Fehérvári Dóra <fdora030925@gmail.com>
2024-12-14 11:37:03 +0100

    feat: set up database connection

--- src/main/java/Webfejlesztes_Projekt/BookRental/Repository/UserRepository.java
+++ src/main/java/Webfejlesztes_Projekt/BookRental/Repository/UserRepository.java
@@ -1,4 +1,9 @@
 package Webfejlesztes_Projekt.BookRental.Repository;
 
-public interface UserRepository {
+import Webfejlesztes_Projekt.BookRental.Entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
 }

75f0d5ffedd593060f067d5683fd3d3a49f88d26
Fehérvári Dóra <fdora030925@gmail.com>
2024-12-14 10:40:13 +0100

    Initial commit

---
+++ src/main/java/Webfejlesztes_Projekt/BookRental/BookRentalApplication.java
@@ -0,0 +1,13 @@
+package Webfejlesztes_Projekt.BookRental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookRentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookRentalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Webfejlesztes_Projekt/BookRental/BookRentalApplicationTests.java
@@ -0,0 +1,13 @@
+package Webfejlesztes_Projekt.BookRental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookRentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
